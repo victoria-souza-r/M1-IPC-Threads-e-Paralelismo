628cf6d18dbea5843d6acb7a750c4daff61773c8
Souza <victoria_souza@edu.univali.br>
2026-04-04 12:54:28 -0300

    pgm.h

--- pgm.h
+++ pgm.h
@@ -1,1 +1,28 @@
+#ifndef PGM_H
+#define PGM_H
+#include <stdlib.h>
 
+// Facilitador para não precisar digitar 'unsigned char' toda vez
+typedef unsigned char uchar;
+
+// Estrutura da Imagem
+typedef struct {
+    int w, h, maxv;
+    uchar* data; // O asterisco indica que é um vetor de pixels
+} PGM;
+
+// Estrutura de Metadados para o FIFO (Conforme Seção 2 do PDF)
+typedef struct {
+    int w, h, maxv;
+    int mode;   // 0=NEGATIVO, 1=SLICE
+    int t1, t2; // Limites para o SLICE (válido se mode=SLICE)
+} Header;
+
+// Protótipos das funções de I/O
+int read_pgm(const char* path, PGM* img);
+int write_pgm(const char* path, const PGM* img);
+
+// Função para liberar a memória alocada dinamicamente
+void free_pgm(PGM* img);
+
+#endif
